2745f902aea9989f4cbc94b99395c85885aaad95
joshua3403 <joshua3403@gmail.com>
2020-05-09 10:37:46 +0900

    1. 1부터 N까지 M배수의 합

---
+++ 1부터 N까지 M배수의 합.cpp
@@ -0,0 +1,24 @@
+
+#include "stdafx.h"
+
+int main()
+{
+	int n, m;
+	n = m = 0;
+	int result = 0;
+
+	scanf("%d", &n);
+	scanf("%d", &m);
+
+	for (int i = 0; i <= n; i++)
+	{
+		if ((i % m) == 0)
+		{
+			result += i;
+		}
+	}
+
+	printf("%d", result);
+
+	return 0;
+}
