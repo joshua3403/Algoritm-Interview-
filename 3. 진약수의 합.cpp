3ecb206baf2b898cc6262cf90e9f666ab8af23d5
joshua3403 <joshua3403@gmail.com>
2020-05-15 10:08:04 +0900

    Revert "Revert "04. 나이차이""
    This reverts commit 64691519831150e9db8bdf2b6a504ff3b4209f26.

---
+++ 3. 진약수의 합.cpp
@@ -0,0 +1,25 @@
+
+#include "stdafx.h"
+
+int main()
+{
+	int n;
+	int result = 1;
+
+	scanf("%d", &n);
+
+	printf("1");
+	for (int i = 2; i < n; i++)
+	{
+		if ((n % i) == 0)
+		{
+			result += i;
+			printf(" + %d", i);
+		}
+	}
+
+	printf(" = %d", result);
+	
+
+	return 0;
+}
--- stdafx.h
+++ stdafx.h
@@ -4,3 +4,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
