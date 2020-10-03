8a7c58cc7f7475feface179ad9a72bc2ec610a1c
Joshua3403 <joshua3403@gmail.com>
2020-10-03 16:39:49 +0900

    no message

--- 83. 복면산 SEND + MORE = MONEY.cpp
+++ 83. 복면산 SEND + MORE = MONEY.cpp
@@ -2,10 +2,81 @@
 
 char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 std::map<char, int> backup;
-char
+std::map<char, bool> check;
+int numbers[10];
+std::string a, b, c;
+
+
+int GetA()
+{
+	int t = 0;
+	for (int i = a.size() - 1; i >= 0; --i)
+	{
+		t += (*backup.find(a[i])).second * pow(10, a.size() - 1 - i);
+	}
+	return t;
+}
+
+int GetB()
+{
+	int t = 0;
+	for (int i = b.size() - 1; i >= 0; --i)
+	{
+		t += (*backup.find(b[i])).second * pow(10, b.size() - 1 - i);
+	}
+	return t;
+}
+
+int GetC()
+{
+	int t = 0;
+	for (int i = c.size() - 1; i >= 0; --i)
+	{
+		t += (*backup.find(c[i])).second * pow(10, c.size() - 1 - i);
+	}
+	return t;
+}
+
+void DFS(int t)
+{
+	if (t == backup.size())
+	{
+		if (GetA() + GetB() == GetC())
+		{
+			if ((*backup.find(a[0])).second == 0 || (*backup.find(b[0])).second == 0 || (*backup.find(c[0])).second == 0)
+				return;
+
+			printf("Find\n");
+		}
+	}
+	else
+	{
+
+		for (std::map<char, int>::iterator itor = backup.begin(); itor != backup.end(); itor++)
+		{
+			if (check[(*itor).first] == false)
+			{
+				check[(*itor).first] = true;
+				for (int i = 0; i < 10; i++)
+				{
+					if (numbers[i] == 0)
+					{
+						numbers[i] = 1;
+						(*itor).second = i;
+						DFS(t + 1);
+						numbers[i] = 0;
+					}
+
+				}
+				check[(*itor).first] = false;
+
+			}
+
+		}
+	}
+}
 int main()
 {
-	std::string a, b, c;
 	std::cin >> a;
 	std::cin >> b;
 	std::cin >> c;
@@ -15,6 +86,7 @@ int main()
 		if (backup.find(a[i]) == backup.end())
 		{
 			backup.insert(std::make_pair(a[i], -1));
+			check.insert(std::make_pair(a[i], false));
 		}
 	}
 
@@ -23,6 +95,7 @@ int main()
 		if (backup.find(b[i]) == backup.end())
 		{
 			backup.insert(std::make_pair(b[i], -1));
+			check.insert(std::make_pair(b[i], false));
 		}
 	}
 
@@ -32,8 +105,11 @@ int main()
 		if (backup.find(c[i]) == backup.end())
 		{
 			backup.insert(std::make_pair(c[i], -1));
+			check.insert(std::make_pair(c[i], false));
 		}
 	}
 
+
+	DFS(0);
 	return 0;
 }
---
+++ 84. 휴가(DFS 활용).cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+int day;
+int main()
+{
+
+
+	return 0;
+}
